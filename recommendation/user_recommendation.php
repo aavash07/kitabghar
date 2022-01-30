fe406df857c1a38ace468cb2c44c1e499b1515a6
aavash07 <aavash07@users.noreply.github.com>
2022-01-30 09:18:52 +0545

    Fixed if user is new

--- recommendation/user_recommendation.php
+++ recommendation/user_recommendation.php
@@ -3,16 +3,21 @@ include 'recommend.php';
 $conn = $pdo->open();
 $stmtrating = $conn->prepare("SELECT * FROM ratings");
 $stmtrating->execute();
-$matrix=array();
-foreach ($stmtrating as $rating)
-{
-	$stmtuser = $conn->prepare("select * from users where id=:user_id");
-	$stmtuser->execute(['user_id' => $rating['user_id']]);
-	$username = $stmtuser->fetch();
+$matrix = array();
+$flag = false;
+if (isset($_SESSION['user'])) {
+    foreach ($stmtrating as $rating) {
+        $stmtuser = $conn->prepare("select * from users where id=:user_id");
+        $stmtuser->execute(['user_id' => $rating['user_id']]);
+        $username = $stmtuser->fetch();
 
-	$matrix[$username['firstname']][$rating['isbn']]=$rating['book_rating'];
-}
+        if ($rating['user_id'] == $user['id'])
+            $flag = true;
 
-if(isset($_SESSION['user'])){
-	$recval=getRecommendation($matrix,$user['firstname']);
+        $matrix[$username['firstname']][$rating['isbn']] = $rating['book_rating'];
+    }
+    if ($flag) {
+        $recval = getRecommendation($matrix, $user['firstname']);
+    }
+
 }
